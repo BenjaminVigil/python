7ce8ce18f2a67865c08e381078f24c2eb7d34ce4
Benjamin Vigil <bvigil3@uccs.edu>
2019-05-28 22:36:41 -0600

    1st lesson with python

---
+++ variable.py
@@ -0,0 +1,20 @@
+variable_name = "Ben"
+variable_age = "21"
+is_male = False
+
+print ("There once was a man named " + variable_name + " and his age was " + variable_age)
+
+variable_name = "Isaiah"
+variable_age = "20"
+
+print ("he had a best friend named " + variable_name + " and his age was " + variable_age)
+
+# this just shows how linear the coding language works - even though it is the same variable since it is lower down on the list it changes it
+
+phrase = "Hello this shows you can store an entire string in a variable"
+
+print(phrase.upper().isupper())
+
+# the .lower or .upper function just makes the string either lower or upper case and you can use fucnctions one after another
+
+print (phrase + "and shows how to add onto") 
